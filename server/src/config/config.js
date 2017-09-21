07675f888122daac6abac97c2fd335b33626ac22
Bryan <x0232947@bryans-mbp.attlocal.net>
2017-09-21 12:23:31 -0500

    added sequlize and sqlite, as well as some configurations and js files

---
+++ server/src/config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  port: process.env.PORT || 8081,
+  db: {
+    database: process.env.DB_NAME || 'tabapp',
+    user: process.env.DB_USER || 'tabapp',
+    password: process.env.DB_PASS || 'tabapp',
+    options: {
+      dialect: process.env.DIALECT || 'sqlite',
+      host: process.env.HOST || 'localhost',
+      storage: './tabapp.sqlite'
+    }
+  }
+}

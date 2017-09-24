2da656d6821a44bfe67b3798d5490ba05a778803
Bryan <x0232947@bryans-mbp.attlocal.net>
2017-09-24 15:17:21 -0500

    added song creation and song viewing

--- client/src/components/CreateSong.vue
+++ client/src/components/CreateSong.vue
@@ -4,35 +4,54 @@
       <panel title="Song Metadata">
         <v-text-field
           label="Title"
+          required
+          :rules="[required]"
           v-model="song.title"></v-text-field>
         <v-text-field
           label="Artist"
+          required
+          :rules="[required]"
           v-model="song.artist"></v-text-field>
         <v-text-field
           label="Genre"
+          required
+          :rules="[required]"
           v-model="song.genre"></v-text-field>
         <v-text-field
           label="Album"
+          required
+          :rules="[required]"
           v-model="song.album"></v-text-field>
         <v-text-field
           label="Album Image Url"
+          required
+          :rules="[required]"
           v-model="song.albumImageURL"></v-text-field>
         <v-text-field
           label="YouTube ID"
+          required
+          :rules="[required]"
           v-model="song.youtubeId"></v-text-field>
       </panel>
     </v-flex>
     <v-flex xs8>
       <panel title="Song Structure" class="ml-2">
         <v-text-field
-            label="Tab"
-            multi-line
-            v-model="song.tab"></v-text-field>
+          label="Tab"
+          multi-line
+          required
+          :rules="[required]"
+          v-model="song.tab"></v-text-field>
         <v-text-field
           label="Lyrics"
           multi-line
+          required
+          :rules="[required]"
           v-model="song.lyrics"></v-text-field>
       </panel>
+      <div class="danger-alert" v-if="error">
+        {{error}}
+      </div>
       <v-btn
         dark
         class="cyan"
@@ -59,11 +78,21 @@ export default {
         youtubeId: null,
         lyrics: null,
         tab: null
-      }
+      },
+      error: null,
+      required: (value) => !!value || 'Required'
     }
   },
   methods: {
     async create () {
+      this.error = null
+      const fieldsFilledIn = Object
+        .keys(this.song)
+        .every(key => !!this.song[key])
+      if (!fieldsFilledIn) {
+        this.error = 'Please fill in all fields'
+        return
+      }
       // call api
       try {
         await SongService.post(this.song)
--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -2,8 +2,38 @@
   <v-layout column>
       <v-flex xs6 offset-xs3>
         <panel title='Songs'>
+          <v-btn
+            slot="action"
+            fab
+            class="cyan accent-2"
+            light
+            small
+            absolute
+            right
+            middle
+            @click="navigateTo({name: 'songs-create'})">
+            <v-icon>add</v-icon>
+          </v-btn>
           <div v-for="song in songs"
+            class="song"
             :key="song.id">
+
+            <v-layout>
+              <v-flex xs6>
+                <div class="song-title">
+                  {{song.title}}
+                </div>
+                <div class="song-artist">
+                  {{song.artist}}
+                </div>
+                <div class="song-genre">
+                  {{song.genre}}
+                </div>
+              </v-flex>
+              <v-flex xs6>
+                <img class="album-image" :src="song.albumImageURL"/>
+              </v-flex>
+            </v-layout>
             {{song.title}} -
             {{song.artist}} -
             {{song.album}}
@@ -26,6 +56,11 @@ export default {
       songs: null
     }
   },
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    }
+  },
   async mounted () {
     // request songs from backend
     // .data is how axios returns the data from the query
@@ -35,4 +70,22 @@ export default {
 </script>
 
 <style scoped>
+  .song {
+    padding: 20px;
+    height: 330px;
+    overflow: hidden;
+  }
+  .song-title {
+    font-size: 30px;
+  }
+  .song-artist {
+    font-size: 24px;
+  }
+  .song-genre {
+    font-size: 18px;
+  }
+  .album-image {
+    width: 70%;
+    margin: 0 auto;
+  }
 </style>
